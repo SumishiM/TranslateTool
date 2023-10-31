5a762f712eb38fd9334f1af9cbaed8e390b58d4b
EicheIIes <149478616+EicheIIes@users.noreply.github.com>
2023-10-31 12:13:58 +0100

    Ajout des packages NewtonSoft et CSVHelper

---
+++ src/TranslateToolAPI/CSVSerializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace TranslateToolAPI
+{
+    internal class CSVSerializer : ISerializable
+    {
+        public T Deserialize<T>(string path)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public T Deserialize<T>(FileStream file)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Serialize<T>(string path, T _obj)
+        {
+
+            throw new NotImplementedException();
+        }
+        
+        public void Serialize<T>(FileStream file, T _obj)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
