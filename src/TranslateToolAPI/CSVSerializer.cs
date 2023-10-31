8a1882517b41d91fa0c3a4eabca8965d0f20222d
EicheIIes <149478616+EicheIIes@users.noreply.github.com>
2023-10-31 12:44:41 +0100

    CSVSerializer fini

--- src/TranslateToolAPI/CSVSerializer.cs
+++ src/TranslateToolAPI/CSVSerializer.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Globalization;
 using CsvHelper;
 using CsvHelper.Configuration;
+using System.IO;
+
 
 namespace TranslateToolAPI
 {
@@ -13,34 +15,43 @@ namespace TranslateToolAPI
     {
         public T Deserialize<T>(string path)
         {
-            throw new NotImplementedException();
+            StreamReader reader = new StreamReader(path);
+            CsvReader Csv = new CsvReader(reader,CultureInfo.InvariantCulture);
+            
+           return Csv.GetRecord<T>();
+            
         }
         
         public T Deserialize<T>(FileStream file)
         {
-            throw new NotImplementedException();
+            StreamReader reader = new StreamReader(file);
+            CsvReader Csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            return Csv.GetRecord<T>();
+            
         }
 
 
         public void Serialize<T>(string path, T _obj)
         {
             StreamWriter writer = new StreamWriter(path);
-            CsvWriter(writer, CultureInfo.InvariantCulture);
+            CsvWriter Csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+               
 
 
+            Csv.WriteField<T>(_obj);
 
-            Csv.WriteRecords(T);
-
-
-
-
-            throw new NotImplementedException();
         }
         
         public void Serialize<T>(FileStream file, T _obj)
         {
 
-            throw new NotImplementedException();
+            StreamWriter writer = new StreamWriter(file);
+            CsvWriter Csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+
+
+            Csv.WriteField<T>(_obj);
         }
     }
 }
