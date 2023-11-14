0bb2e013c8fdd55a14c97e657c07166a6b2d7990
EicheIIes <149478616+EicheIIes@users.noreply.github.com>
2023-11-14 10:33:03 +0100

    Cpp and Cs parser finished

--- src/TranslateToolAPI/CppParser.cs
+++ src/TranslateToolAPI/CppParser.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,86 @@ namespace TranslateToolAPI
 {
     internal class CppParser
     {
+        private StreamWriter _writer;
         public void Parse<T>(string path, T obj)
         {
+            _writer = new StreamWriter(path + "TranslateItem.h");
+            _writer.WriteLine("#include <std::map>");
+            _writer.WriteLine("Namespace TranslateToolAPI");
+            _writer.WriteLine("{");
+            _writer.WriteLine("Class TranslateItem {");
+            _writer.WriteLine("Public :");
+            _writer.WriteLine("int Id;");
+            _writer.WriteLine("std::map<std::string, std::string> Translation;");
 
+
+
+            _writer.WriteLine("}");
+            _writer.WriteLine("}");
+
+
+
+            _writer = new StreamWriter(path + "TranslateItem.Cpp");
+
+            _writer.WriteLine("#include <" + path + " TranslateItem.h>");
+            _writer.WriteLine("Namespace TranslateToolAPI");
+            _writer.WriteLine("{");
+            _writer.WriteLine("void main()");
+            _writer.WriteLine("{");
+
+
+            _writer.WriteLine("}");
+            _writer.WriteLine("}");
+
+
+
+            _writer = new StreamWriter(path + "Generated_Cpp.h");
+
+            _writer.WriteLine("#include <std::map>");
+            _writer.WriteLine("#include <std::list>");
+            _writer.WriteLine("Class TranslateItem;");
+
+
+            _writer.WriteLine("Namespace TranslateToolAPI");
+            _writer.WriteLine("{");
+            _writer.WriteLine("Class Generated_CS {");
+            _writer.WriteLine("Public :");
+            _writer.WriteLine("std::list<std::map> Translations;");
+            _writer.WriteLine("Generated_Cs::Generated_Cs();");
+
+
+
+            _writer.WriteLine("}");
+            _writer.WriteLine("}");
+
+
+
+
+
+
+
+            _writer = new StreamWriter(path + "Generated_Cpp.cpp");
+            _writer.WriteLine("#include <" + path + "Generated_Cpp.h>");
+            _writer.WriteLine("#include <" + path + "TranslateItem.h>");
+            _writer.WriteLine("Namespace TranslateToolAPI");
+            _writer.WriteLine("{");
+            _writer.WriteLine("Generated_Cs::Generated_Cs()");
+            _writer.WriteLine("{");
+            for (int i = 0; i < Application.translateItems.Count; i++)
+            {
+                _writer.WriteLine("Translations.Add(new TranslateItem());");
+                _writer.WriteLine("Translations[" + i + "].Id = " + i + ";");
+                _writer.WriteLine(" Translations[" + i + "].Translation = new std::map<std::string, std::string>() {  ");
+
+                _writer.WriteLine("{\"fr_FR\", " + Application.translateItems.ElementAt(i).Translations["fr_FR"] + " },");
+                _writer.WriteLine("{\"en_US\", " + Application.translateItems.ElementAt(i).Translations["en_US"] + " },");
+                _writer.WriteLine("{\"es_ES\", " + Application.translateItems.ElementAt(i).Translations["es_ES"] + " },");
+
+                _writer.WriteLine("};");
+            }
+
+            _writer.WriteLine("}");
+            _writer.WriteLine("}");
         }
     }
 }
--- src/TranslateToolAPI/CsParser.cs
+++ src/TranslateToolAPI/CsParser.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using TranslateToolAPI;
 
 namespace TranslateToolAPI
 {
@@ -12,7 +13,7 @@ namespace TranslateToolAPI
         private StreamWriter _writer;
         public void Parse<T>(string path, T obj)
         {
-            _writer = new StreamWriter(path);
+            _writer = new StreamWriter(path + "Generated_CS.cs");
 
             _writer.WriteLine("using System;");
             _writer.WriteLine("using System.Collections.Generic;");
@@ -26,8 +27,16 @@ namespace TranslateToolAPI
 
             for (int i = 0; i < Application.translateItems.Count; i ++)
             {
-                _writer.WriteLine("Id.Add(" + Application.translateItems.ElementAt(i).Id +")");
-                //_writer.WriteLine();
+                _writer.WriteLine("TranslateItems.Add(new TranslateItem());");
+                _writer.WriteLine(" TranslateItems["+i+"].Id = "+i+";");
+                _writer.WriteLine("TranslateItems["+i+"].Translations = new Dictionary<string, string>()");
+                _writer.WriteLine("{");
+                _writer.WriteLine(" {" + "\"fr_FR\","+ "\"" + Application.translateItems.ElementAt(i).Translations["fr_FR"] + "\" }," );
+                _writer.WriteLine(" {" + "\"en_US\","+ "\"" + Application.translateItems.ElementAt(i).Translations["en_US"] + "\" }," );
+                _writer.WriteLine(" {" + "\"es_ES\","+ "\"" + Application.translateItems.ElementAt(i).Translations["es_ES"] + "\" }," );
+                _writer.WriteLine(" }");
+                _writer.WriteLine(";");
+
 
 
             }
@@ -38,18 +47,20 @@ namespace TranslateToolAPI
         }
     }
 }
-
+//"fr_FR" "en_US" "es_ES"
 internal partial class Generated_CS
 {
-        public List<uint> Id { get; set; }
-    public Dictionary<string, string> Translations { get; set; }
+        
+    public List<TranslateItem> TranslateItems { get; set; }
     
    
 
     Generated_CS() 
     {
-    
-    
+
+        TranslateItems.Add(new TranslateItem());
+        TranslateItems[0].Id = 0;
+        TranslateItems[0].Translations = new Dictionary<string, string>() { { "truc", "haha" } };
     
     }
  
